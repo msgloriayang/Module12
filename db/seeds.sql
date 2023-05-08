840b8f7eab78955a9bc2192cf2862ad9c148ec98
msgloriayang <yanggloria91@gmail.com>
2023-05-08 00:35:48 -0500

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,16 @@
 INSERT INTO department (dept_name)
 VALUES 
-('Parks and Recreation'),
-('Budget and Finance'),
-('City Planning');
-
+('Engineering'),
+('Finance'),
+('Legal');
 
 INSERT INTO job (title, salary, department_id)
 VALUES 
-('Director', 75000, 1),
-('Deputy Director', 45000, 1),
-('Assistant', 18000, 1),
-('Head Auditor', 100000, 2),
-('Accountant', 80000, 2),
-('Engineer', 55000, 3),
-('Miniature Horse', 0, 1);
+('Software Engineer', 75000, 1),
+('Account Manager', 45000, 1),
+('Accountant', 18000, 1),
+('Legal Team Lead', 100000, 2),
+('Lawyer', 80000, 2),
 
 INSERT INTO employee (first_name, last_name, job_id, manager_id)
 VALUES 
