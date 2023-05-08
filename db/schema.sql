238dd968cf79aab228cf9d11d345423a77109c0c
msgloriayang <yanggloria91@gmail.com>
2023-05-08 00:41:13 -0500

    fixed lines 15-20 in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE department (
 CREATE TABLE job (
   id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
   department_id INT
 );
 
